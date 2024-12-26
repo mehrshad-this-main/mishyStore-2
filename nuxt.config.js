e2e1a3674192e16896c3871c0fb555d70ffc08b7
mehrshad <144066822+mehrshad-this-main@users.noreply.github.com>
2024-12-27 00:23:43 +0330

    config

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,6 +46,7 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    rtl: true,
     theme: {
       dark: true,
       themes: {
