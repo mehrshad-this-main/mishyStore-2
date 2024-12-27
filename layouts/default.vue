7262c88e178dc891f316a2c6c72bebf01d275715
mehrshad <144066822+mehrshad-this-main@users.noreply.github.com>
2024-12-27 23:07:43 +0330

    add header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,58 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const toggle = ref(null);
+const navMenu = [
+  { name: "صفحه اصلی", link: "/" ,id:0},
+  { name: "فروشگاه", link: "/",id:1 },
+  { name: "وبلاگ", link: "/",id:2 },
+  { name: "درباره ما", link: "/",id:3 },
+  { name: "تماس باما", link: "/",id:4 },
+];
+</script>
 <template>
   <v-app>
     <div>
       <v-app-bar>
         <h1 class="font-weight-regular text-h5">mishyStore</h1>
-        <div class="d-flex align-center mx-4">
-        <v-text-field
-          hide-details
-          rounded
-          variant="outline"
-          density="compact"
-          :width="300"
-          label="Search"
-        ></v-text-field>
-      </div>
+        <v-responsive max-width="500" class="mx-auto">
+          <v-text-field
+            light
+            class="white"
+            append-icon="mdi-magnify"
+            placeholder="جستوجو کنید..."
+            hide-details
+            rounded
+          />
+        </v-responsive>
+        <div>
+          <v-chip
+            class="ma-2"
+            color="indigo"
+            text-color="white"
+            variant="outlined"
+          >
+            <v-icon>mdi-account</v-icon>
+            <h4 class="text-body-2 font-weight-light">ورود و عضویت</h4>
+          </v-chip>
+          <v-icon>mdi-cart</v-icon>
+        </div>
       </v-app-bar>
+      <v-container class="mt-2 white rounded-r-pill">
+        <v-row class="ma-2 align-center">
+          <div class="pa-2 text-body-1 font-weight-regular primary categoryM">
+            <v-icon> mdi-arrow-down </v-icon>
+            دسته بندی محصولات
+          </div>
+          <v-btn-toggle v-model="toggle" group class="transparent">
+            <v-sheet
+              v-for="item in navMenu"
+              :key="item.id"
+              class="transparent text-body-1 mx-4"
+              ><NuxtLink class="nav" :to="item.link">{{ item.name }}</NuxtLink></v-sheet
+            >
+          </v-btn-toggle>
+        </v-row>
+      </v-container>
     </div>
     <v-main>
       <v-container>
@@ -23,3 +61,15 @@
     </v-main>
   </v-app>
 </template>
+<style>
+.categoryM {
+  border: 2px solid #000;
+  border-radius: 25px;
+  padding: 20px;
+  font-weight: 600;
+}
+.nav{
+  text-decoration: none;
+  font-weight: 600;
+}
+</style>
